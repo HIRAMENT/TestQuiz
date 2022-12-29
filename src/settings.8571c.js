aa7deaf548a04a5354f47ed4b549245c5bb92a94
HIRAMENT <c>
2022-12-29 17:00:22 +0900

    desctopother

---
+++ src/settings.8571c.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/helloworld.fire",orientation:"",jsList:[],bundleVers:{internal:"7f7d5",main:"6cfe4"}};
